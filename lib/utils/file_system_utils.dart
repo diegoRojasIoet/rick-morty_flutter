6519e5557491017a5901afc31e5b15aef3415060
Diego Rojas <drojas@seatgeek.com>
2024-03-24 13:27:53 -0500

    feat: finish character details screen

--- lib/utils/file_system_utils.dart
+++ lib/utils/file_system_utils.dart
@@ -1,20 +1,20 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:path_provider/path_provider.dart';
+import 'package:path_provider/path_provider.dart';
 
-// class FileSystemUtils {
-//   static Future<Directory> createLocalDirectory(String folderName) async {
-//     final Directory? appDocDir = await getExternalStorageDirectory();
+class FileSystemUtils {
+  static Future<Directory> createLocalDirectory(String folderName) async {
+    final Directory? appDocDir = await getExternalStorageDirectory();
 
-//     final Directory appDocDirFolder =
-//         Directory('${appDocDir?.path}/$folderName/');
+    final Directory appDocDirFolder =
+        Directory('${appDocDir?.path}/$folderName/');
 
-//     if (await appDocDirFolder.exists()) {
-//       return appDocDirFolder;
-//     } else {
-//       final Directory appDocDirNewFolder =
-//           await appDocDirFolder.create(recursive: true);
-//       return appDocDirNewFolder;
-//     }
-//   }
-// }
+    if (await appDocDirFolder.exists()) {
+      return appDocDirFolder;
+    } else {
+      final Directory appDocDirNewFolder =
+          await appDocDirFolder.create(recursive: true);
+      return appDocDirNewFolder;
+    }
+  }
+}
--- lib/views/character_details.dart
+++ lib/views/character_details.dart
@@ -1,11 +1,11 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:image_picker/image_picker.dart';
-// import 'package:path/path.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty_app/model/character_model.dart';
-// import 'package:rick_and_morty_app/utils/file_system_utils.dart';
+import 'package:rick_and_morty_app/utils/file_system_utils.dart';
 import 'package:rick_and_morty_app/utils/utils.dart';
 
 class CharacterDetails extends StatelessWidget {
@@ -89,40 +89,40 @@ class _CharacterHeader extends StatelessWidget {
               height: 160,
               width: 180,
             )),
-            // Positioned(
-            //     top: 0,
-            //     right: 0,
-            //     child: IconButton(
-            //       icon: const Icon(Icons.camera_alt_rounded),
-            //       color: Colors.white,
-            //       iconSize: 50,
-            //       onPressed: () async {
-            //         final picker = ImagePicker();
-            //         final XFile? pickedFile = await picker.pickImage(
-            //           source: ImageSource.camera,
-            //         );
-            //         if (pickedFile == null) return;
+            Positioned(
+                top: 0,
+                right: 0,
+                child: IconButton(
+                  icon: const Icon(Icons.camera_alt_rounded),
+                  color: Colors.white,
+                  iconSize: 50,
+                  onPressed: () async {
+                    final picker = ImagePicker();
+                    final XFile? pickedFile = await picker.pickImage(
+                      source: ImageSource.camera,
+                    );
+                    if (pickedFile == null) return;
 
-            //         Directory capturesDirectory =
-            //             await FileSystemUtils.createLocalDirectory(
-            //                 'character_${character.id}');
+                    Directory capturesDirectory =
+                        await FileSystemUtils.createLocalDirectory(
+                            'character_${character.id}');
 
-            //         String fileName = basename(pickedFile.path);
+                    String fileName = basename(pickedFile.path);
 
-            //         String destinationFile =
-            //             '${capturesDirectory.path}$fileName';
+                    String destinationFile =
+                        '${capturesDirectory.path}$fileName';
 
-            //         File sourceFile = File(pickedFile.path);
-            //         File destination = File(destinationFile);
+                    File sourceFile = File(pickedFile.path);
+                    File destination = File(destinationFile);
 
-            //         await destination
-            //             .writeAsBytes(await sourceFile.readAsBytes());
+                    await destination
+                        .writeAsBytes(await sourceFile.readAsBytes());
 
-            //         print("Saved Image Path: $destinationFile");
-            //         print(
-            //             "Saved Image Size: ${await File(destinationFile).length()} bytes");
-            //       },
-            //     ))
+                    print("Saved Image Path: $destinationFile");
+                    print(
+                        "Saved Image Size: ${await File(destinationFile).length()} bytes");
+                  },
+                ))
           ],
         ));
   }
